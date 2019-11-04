5109ba04922ad1a71ca8c3d15e204c9f83037629
Potyashin <48630110+Potyashin@users.noreply.github.com>
2019-11-04 14:11:29 +0300

    k_stat_two_strs(mod 2 task C)

--- main.cpp
+++ main.cpp
@@ -1,282 +1,202 @@
-#pragma optimize("g", off)
-
 #include <iostream>
 #include <vector>
-#include <array>
 #include <string>
 #include <climits>
 #include <algorithm>
-#include <unordered_map>
 
 
 
 
-const int alphabet_size = 28;
-const int char_offset = 95;
+
+const int64_t alphabet_size = 256;
+const int64_t char_a = 0;
 
 
-class SuffixTree {
-private:
-    struct Node {
-        Node();
-        Node(size_t _left, size_t _right, int _parent = -1, int _link = -1);
-        // На ребре хранится подстрока [left, right)
-        size_t left = 0;
-        size_t right = 0;
-        int parent = -1;
-        int link = -1;
-        std::array<int, alphabet_size> edges;
-    };
 
-    struct ActivePtr {
-        size_t vertex = 0;
-        unsigned char edge = 0;
-        size_t length = 0;
-    };
-
-public:
-    SuffixTree();
-    SuffixTree(const std::string& str);
-    void add_symbol(unsigned char sym);
-    Node get_vertex(size_t v) const { return tree[v]; }
-    size_t get_length() const { return text.length(); }
-private:
-    std::string text;
-
-    ActivePtr active_ptr;
-    size_t remainder = 0;
-    int waiting_suf_link = 0; // Вершина, из которой нужно будет кинуть ссылку
-    std::vector<Node> tree;
-
-    void make_suf_link(size_t node_for_link); // Кидает ссылку из запомненной в node_for_link
-    void go_down(bool& flag); // Спускается вниз, пока active_length не будет больше длины текущего ребра
-    // И если спустились, то flag = true
-
-    size_t active_edge_length();  // Длина ребра из активной вершины по активному ребру
-    size_t edge_length(size_t node, unsigned char edge);
-};
-
-SuffixTree::Node::Node(size_t _left, size_t _right, int _parent, int _link) :
-        left(_left),
-        right(_right),
-        parent(_parent),
-        link(_link) {
-    for (size_t i = 0; i < alphabet_size; ++i) {
-        edges[i] = -1;
+int64_t sort_letters(
+        const std::string& str,
+        std::vector<size_t>& sorted_suffixes,
+        std::vector<size_t>& classes) {
+    std::vector<size_t> count(alphabet_size, 0);
+    for (auto ch: str) {
+        ++count[ch - char_a];
     }
-}
-
-SuffixTree::Node::Node() {
-    for (size_t i = 0; i < alphabet_size; ++i) {
-        edges[i] = -1;
+    for (size_t i = 1; i < alphabet_size; ++i) {
+        count[i] += count[i - 1];
     }
-}
-
-SuffixTree::SuffixTree() {
-    Node root;
-    root.link = 0;
-    tree.push_back(root);
-}
-
-SuffixTree::SuffixTree(const std::string& str) : SuffixTree() {
-    for (auto i: str) {
-        add_symbol(i);
+    for (int64_t i = str.length() - 1; i >= 0; --i) {
+        sorted_suffixes[--count[str[i] - char_a]] = i;
     }
-    add_symbol('$');
-}
-
-void SuffixTree::make_suf_link(size_t node_for_link) {
-    if (waiting_suf_link > 0) {
-        tree[waiting_suf_link].link = node_for_link;
+    size_t current_class = 0;
+    classes[sorted_suffixes[0]] = 0;
+    for (size_t i = 1; i < str.length(); ++i) {
+        if (str[sorted_suffixes[i]] != str[sorted_suffixes[i - 1]]) {
+            ++current_class;
+        }
+        classes[sorted_suffixes[i]] = current_class;
     }
-    waiting_suf_link = node_for_link;
+    return current_class;
 }
 
-size_t SuffixTree::active_edge_length() {
-    auto edge = tree[active_ptr.vertex].edges[active_ptr.edge - char_offset];
-    if (edge == -1)
-        return INT_MAX;
-    return tree[edge].right - tree[edge].left;
-}
-
-size_t SuffixTree::edge_length(size_t node, unsigned char edge) {
-    auto q = tree[node].edges[edge - char_offset];
-    return tree[q].right - tree[q].left;
-}
-
-void SuffixTree::go_down(bool& flag) {
-    while (active_edge_length() <= active_ptr.length) {
-        active_ptr.length -= active_edge_length();
-        int length = active_edge_length();
-        size_t old_active_node = active_ptr.vertex;
-        active_ptr.vertex = tree[old_active_node].edges[active_ptr.edge - char_offset];
-        if (active_ptr.length == 0) {
-            active_ptr.edge = char_offset;
+std::vector<size_t> sort_by_second_parts (
+        const std::string& str,
+        int64_t current_length,
+        const std::vector<size_t>& sorted_suffixes) {
+    std::vector<size_t> result(str.length());
+    for (size_t i = 0; i < str.length(); ++i) {
+        if (sorted_suffixes[i] < current_length) {
+            // Случай "зацикливания"
+            result[i] = str.length() + sorted_suffixes[i] - current_length;
         }
         else {
-            active_ptr.edge = text[text.size() - 1 - active_ptr.length];
-        }
-
-        flag = true;
-    }
-}
-
-void SuffixTree::add_symbol(unsigned char sym) {
-    ++remainder;
-    text += sym;
-    while (remainder > 0) {
-        // На каждом шаге хотим добавить sym в суффикс длины remainder
-        if (active_ptr.length == 0) {
-            active_ptr.edge = sym;
-        }
-        // Если из active_ptr нет нужного ребра, то мы создаём лист
-        if (tree[active_ptr.vertex].edges[active_ptr.edge - char_offset] == -1) {
-            Node leaf(text.size() - 1, INT_MAX, active_ptr.vertex, -1);
-            tree.push_back(leaf);
-            tree[active_ptr.vertex].edges[active_ptr.edge - char_offset] = tree.size() - 1;
-            // Не забываем кидать суффикную ссылку
-            make_suf_link(active_ptr.vertex);
-        }
-        else {
-
-            size_t next_vertex = tree[active_ptr.vertex].edges[active_ptr.edge - char_offset];
-
-            bool did_go_down = false;
-            go_down(did_go_down);
-            if (did_go_down) {
-                continue;
-            }
-            // Если символ уже на ребре, мы всего лишь изменяем active_length
-            if (text[tree[next_vertex].left + active_ptr.length] == sym) {
-                ++active_ptr.length;
-                if (waiting_suf_link > 0) {
-                    make_suf_link(active_ptr.vertex);
-                }
-                break;
-            }
-
-            // Иначе мы находимся где-то на ребре и сплитим его
-            size_t new_node_index = tree.size();
-            size_t new_leaf_index = new_node_index + 1;
-
-            auto edge = tree[next_vertex];
-            Node new_node(edge.left, edge.left + active_ptr.length, active_ptr.vertex, -1);
-            tree.push_back(new_node);
-
-            tree[active_ptr.vertex].edges[active_ptr.edge - char_offset] = new_node_index;
-            tree[next_vertex].left += active_ptr.length;
-
-            Node new_leaf(text.size() - 1, INT_MAX, new_node_index, -1);
-            tree.push_back(new_leaf);
-
-            tree[new_node_index].parent = active_ptr.vertex;
-            tree[new_node_index].edges[sym - char_offset] = new_leaf_index;
-            tree[new_node_index].edges[text[new_node.right] - char_offset] = next_vertex;
-
-            tree[new_leaf_index].parent = new_node_index;
-            tree[next_vertex].parent = new_node_index;
-
-            make_suf_link(new_node_index);
-        }
-
-        --remainder;
-
-        // Если active_ptr в root'е, то мы его готовим у следующему суффиксу
-        if (active_ptr.vertex == 0 && active_ptr.length > 0) {
-            --active_ptr.length;
-            active_ptr.edge = text[text.size() - remainder];
-        }
-            // Иначе мы переходим по суффиксной ссылке
-        else {
-            int link = tree[active_ptr.vertex].link;
-            active_ptr.vertex = link > 0 ? link : 0;
-        }
-
-    }
-    waiting_suf_link = 0;
-}
-
-struct VertexDescription {
-    size_t p = 0; // родитель
-    size_t w = 0; // первая или вторая строка
-    size_t lf = 0; // левая граница строки
-    size_t rg = 0; // правя граница
-};
-
-void describe_tree (  // Выписывает вершины дерева
-        const SuffixTree& tree,
-        size_t vertex,
-        std::vector<VertexDescription>& result,
-        size_t first_length,
-        size_t second_length,
-        std::unordered_map<size_t, size_t>& indexes
-) {
-    for (size_t i = 0; i < alphabet_size; ++i) {
-        if (tree.get_vertex(vertex).edges[i] != -1) {
-            auto next_vertex = tree.get_vertex(tree.get_vertex(vertex).edges[i]);
-            VertexDescription current;
-            current.p = indexes[next_vertex.parent];
-            current.lf = next_vertex.left;
-            if (next_vertex.left < first_length) {
-                current.w = 0;
-            }
-            else {
-                current.w = 1;
-            }
-
-            if (current.w == 1) {
-                current.rg = std::min(next_vertex.right, tree.get_length()) - first_length;
-                current.lf -= first_length;
-            }
-            else {
-                current.rg = std::min(next_vertex.right, first_length);
-            }
-            result.push_back(current);
-            indexes[tree.get_vertex(vertex).edges[i]] = result.size();
-            describe_tree(tree, tree.get_vertex(vertex).edges[i], result, first_length, second_length, indexes);
+            result[i] = sorted_suffixes[i] - current_length;
         }
     }
-}
-
-std::vector<VertexDescription> tree_from_two_string(const std::string& first, const std::string& second) {
-    SuffixTree tree;
-    for (auto i: first) {
-        tree.add_symbol(i);
-    }
-    for (auto i: second) {
-        tree.add_symbol(i);
-    }
-    std::vector<VertexDescription> result;
-    std::unordered_map<size_t, size_t> indexes;
-    indexes[0] = 0;
-    describe_tree(tree, 0, result, first.length(), second.length(), indexes);
     return result;
 }
 
+void count_sort(
+        const std::string& str,
+        const std::vector<size_t>& sorted_by_second_parts,
+        int64_t current_length,
+        std::vector<size_t>& sorted_suffixes,
+        std::vector<size_t>& classes,
+        int64_t& maxclass) {
+    std::vector<size_t> new_classes(str.length());
+    std::vector<size_t> count(maxclass + 1, 0);
+
+    for (size_t i = 0; i < str.length(); ++i) {
+        ++count[classes[sorted_by_second_parts[i]]];
+    }
+    for (size_t i = 1; i < maxclass + 1; ++i) {
+        count[i] += count[i - 1];
+    }
+    for (int64_t i = str.length() - 1; i >= 0; --i) {
+        if (i == 12) {
+            int a = 2;
+        }
+        size_t index = sorted_by_second_parts[i];
+        sorted_suffixes[--count[classes[index]]] = index;
+    }
+    new_classes[sorted_suffixes[0]] = 0;
+    maxclass = 0;
+    for (size_t i = 1; i < str.length(); ++i) {
+        size_t first1 = sorted_suffixes[i];
+        size_t first2 = sorted_suffixes[i - 1];
+        size_t second1 = (first1 + current_length) % str.length();
+        size_t second2 = (first2 + current_length) % str.length();
+
+        if ((classes[first1] != classes[first2]) || (classes[second1] != classes[second2])) {
+            ++maxclass;
+        }
+        new_classes[sorted_suffixes[i]] = maxclass;
+    }
+    classes = new_classes;
+}
+
+std::vector<size_t> suffix_array(const std::string& str) {
+    std::vector<size_t> sorted_suffixes(str.length());  // Номера суффиксов, отсортированных лексикографически
+    std::vector<size_t> classes(alphabet_size + str.length(), 0); // Для хранения классов эквивалентности
+
+    // Отсортируем циклические подстроки длины один
+    int64_t maxclass = sort_letters(str, sorted_suffixes, classes);
+
+    // Теперь индуктивно строим для подстрок длины 2^k
+    for (size_t current_length = 1; current_length < str.length(); current_length*=2) {
+        // Сформируем сортированную по второй половине суффикса длины 2*current_length последовательность
+        auto sorted_by_second_parts = sort_by_second_parts(str, current_length, sorted_suffixes);
+
+        // И теперь сортируем подсчётом по левой половине
+        count_sort(str, sorted_by_second_parts, current_length, sorted_suffixes, classes, maxclass);
+        // В classes и sorted_suffixes теперь лежат новые значения
+    }
 
 
+    return sorted_suffixes;
+}
+
+std::vector<int64_t> lcp_on_suff_array(const std::string& str, const std::vector<size_t>& suf) {
+    std::vector<int64_t> lcp(str.length());
+    std::vector<size_t> positions(str.length()); // обратный к suf
+
+    for (size_t i = 0; i < positions.size(); ++i) {
+        positions[suf[i]] = i;
+    }
+
+    size_t k = 0;
+    for (size_t i = 0; i < str.length(); ++i) {
+        if (k > 0) {
+            --k;
+        }
+        if (positions[i] == str.length() - 1) {
+            lcp[str.length() - 1] = -1;
+            k = 0;
+            continue;
+        }
+        else {
+            int64_t j = suf[positions[i] + 1];
+            while (std::max(i + k, j + k) < str.length() && str[i + k] == str[j + k]) {
+                ++k;
+            }
+            lcp[positions[i]] = k;
+        }
+    }
+    return lcp;
+}
+
+bool are_in_different_strs (
+        int64_t index1,
+        int64_t index2,
+        const std::string& first) {
+    return ((index1 > first.length() && index2 < first.length()) ||
+            (index2 > first.length() && index1 < first.length()));
+}
+
+std::string k_stat(const std::string& first, const std::string& second, int64_t k) {
+    auto concatenated = first + '#' + second + '$';
+    auto suf = suffix_array(concatenated);
+    auto lcp = lcp_on_suff_array(concatenated, suf);
+    std::string result;
+    int64_t zero = 0;
+    int64_t min = 0;
+    for (int64_t i = 2; i < suf.size() - 1; ++i) {
+        if (are_in_different_strs(suf[i], suf[i + 1], first) && lcp[i] != 0) {
+            int64_t current_subtract = std::max(lcp[i] - min, zero);
+            if (k <= current_subtract) {
+                result = concatenated.substr(suf[i], k + min);
+                k = 0;
+                break;
+            }
+            else {
+                k -= current_subtract;
+                min = lcp[i];
+            }
+        }
+        min = std::min(min, lcp[i]);
+    }
+    if (k > 0) {
+        result = "-1";
+    }
+    return result;
+}
 
 int main() {
 
     std::string first;
     std::string second;
-
+    int64_t k;
     std::cin >> first;
-
-    SuffixTree tree(first);
-
-    first.back() = 96;
     std::cin >> second;
-    second.back() = 95;
-
-    auto result = tree_from_two_string(first, second);
-    std::cout << result.size() + 1 << std::endl;
-    for (auto i: result) {
-        std::cout << i.p << " "
-                  << i.w << " "
-                  << i.lf << " "
-                  << i.rg << "\n";
+    std::cin >> k;
+    if (k == 0) {
+        std::cout << -1;
+        return 0;
     }
+    if (first.length() == 0 || second.length() == 0) {
+        std::cout << -1;
+        return 0;
+    }
+    std::cout << k_stat(first, second, k);
 
     return 0;
 }
